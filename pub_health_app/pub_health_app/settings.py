594593ad88f6c36f67d55151c8e202358ff1380c
Busch <josefine.busch@posteo.de>
2022-11-09 16:53:39 +0100

    Moved secret key to environment variables. Install dependency django-environment and get file with key from Josefine.

--- pub_health_app/pub_health_app/settings.py
+++ pub_health_app/pub_health_app/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+# reading .env file
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gfeyiyu_k(jeo0xkl9-c@ksukixjjmc_wio+zee%!li1e4wkyh'
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
