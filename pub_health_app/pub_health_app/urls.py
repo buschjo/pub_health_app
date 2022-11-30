f793cb347e96ccaeeb393861503704e0a38dc4a0
Busch <josefine.busch@posteo.de>
2022-11-30 16:15:14 +0100

    added url pattern for admin environment back ignored

--- pub_health_app/pub_health_app/urls.py
+++ pub_health_app/pub_health_app/urls.py
@@ -26,6 +26,7 @@ Including another URLconf
 from django.urls import include, path
 from rest_framework import routers
 from trips import views
+from django.contrib import admin
 
 router = routers.DefaultRouter()
 router.register(r'trips', views.TripViewSet)
@@ -34,5 +35,6 @@ router.register(r'trips', views.TripViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('admin/', admin.site.urls),
 ]
